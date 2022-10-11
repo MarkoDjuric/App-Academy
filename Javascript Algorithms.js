72f03cf76f28bc290f9f70e46fe116f7cd5f9e93
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2022-10-11 22:31:50 +0200

    Update Javascript Algorithms.js

--- Javascript Algorithms.js
+++ Javascript Algorithms.js
@@ -10,12 +10,9 @@
 function logBetweenStepper(min, max, step) {
    
    let i = min;
-   while (i <= max) {
-       
+   while (i <= max) {    
        i += step;
-       
-   }
-    
+   }   
 }
 
 
@@ -62,4 +59,33 @@ console.log(
 console.log(wordWithinArray(
   ["dog", "cat", "camel", "bird"], "panther")
 ); //=> false
+
+
+//Add To Array - Research
+//Write a function that takes a location, either "FRONT" or "BACK" and adds an element to either the front or back of the given array.
+//If location is anything besides "FRONT" or "BACK", print an error. Your function should not return anything and should mutate the original array. (Hint: Look up the JavaScript functions: push/pop/shift/unshift)
+
+
+function addToArray(location, element, arr) {
+    // Your code here
+if (location === "FRONT") {
+  console.log(arr.unshift(element))
+}else if (location === "BACK") {
+ console.log( arr.push(element)) 
+}else if(location === "MIDDLE"){
+  console.log("ERROR") 
+}
+}
+testArray = [1,2,3]
+
+addToArray("FRONT", 0, testArray)
+console.log(testArray) // [0,1,2,3]
+
+addToArray("BACK", 4, testArray)
+console.log(testArray) // [0,1,2,3,4]
+
+addToArray("MIDDLE", 4, testArray) // "ERROR"
+console.log(testArray) // [0,1,2,3,4]
+
+
 
