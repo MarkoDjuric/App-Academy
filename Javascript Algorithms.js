f501f337ea1c0bdb60b446f9b58fcd38b38c09ba
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2022-10-12 23:40:44 +0200

    Update Javascript Algorithms.js

--- Javascript Algorithms.js
+++ Javascript Algorithms.js
@@ -108,3 +108,25 @@ console.log(reverseString('fish')); // 'hsif'
 console.log(reverseString('marathon')); // 'nohtaram'
 
 
+//Range
+//Write a function range(min, max) that takes in two numbers. The function should return an array containing all numbers from min to max inclusive. 
+
+// your code here
+
+function range(min, max) {
+ let start = min;
+  let end = max
+  rangeArray = [];
+  while(start <= end) {
+    rangeArray.push(start)
+    start ++;
+  }
+  return rangeArray;
+  
+}
+
+console.log(range(3, 10)); // [ 3, 4, 5, 6, 7, 8, 9, 10 ]
+console.log(range(217, 220)); // [ 217, 218, 219, 220 ]
+console.log(range(-5, 1)); // [ -5, -4, -3, -2, -1, 0, 1 ]
+console.log(range(10, 3)); // []
+
