bc1ceb1c1cfe3733b7e57e21176725be6b47545b
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2022-10-18 23:36:34 +0200

    Update Javascript Algorithms.js

--- Javascript Algorithms.js
+++ Javascript Algorithms.js
@@ -161,3 +161,33 @@ return sum;
 
 console.log(sumArray([5, 6, 4])); // => 15
 console.log(sumArray([7, 3, 9, 11])); // => 30
+
+//Factors Of
+//Write a function factorsOf(num) that takes in a number as an arg. 
+//The method should return an array containing all positive numbers that are able to divide into num with no remainder. 
+
+// your code here
+
+function factorsOf(num) {
+  let factors = [];
+  let i = 1;
+  
+  while (i <= num){
+    
+    if (num % i == 0) {
+     factors.push(i) 
+    }
+    
+    i++;
+  }
+  
+  return factors;
+}
+
+console.log(factorsOf(5)); // [ 1, 5 ]
+console.log(factorsOf(8)); // [ 1, 2, 4, 8 ]
+console.log(factorsOf(9)); // [ 1, 3, 9 ]
+console.log(factorsOf(10)); // [ 1, 2, 5, 10 ]
+console.log(factorsOf(24)); // [ 1, 2, 3, 4, 6, 8, 12, 24 ]
+console.log(factorsOf(2017)); // [ 1, 2017 ]
+
