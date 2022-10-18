ee910dc5bbc3cc52007664f5c411a71d28fa4be5
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2022-10-18 23:38:38 +0200

    Update Javascript Algorithms.js

--- Javascript Algorithms.js
+++ Javascript Algorithms.js
@@ -210,4 +210,28 @@ console.log(myIndexOf(['a', 'b', 'c', 'e'], 'e')); // 3
 console.log(myIndexOf(['a', 'b', 'c', 'e'], 'z')); // -1
 console.log(myIndexOf([43, -7, 11, 13, 43], 43)); // 0
 console.log(myIndexOf([43, -7, 11, 13], 1)); // -1
-//
+
+
+//First Vowel
+//Write a function firstVowel(str) that takes in a string and returns the first vowel that appears sequentially in the string.
+
+// your code here
+function firstVowel(str) {
+  let vowels = "aeiou";
+  
+  for (let x of str) {
+      if (vowels.includes(x)) {
+        return x
+    
+      } 
+  }
+  return null
+  
+}
+
+
+console.log(firstVowel('battery')); // 'a'
+console.log(firstVowel('tunnel')); // 'u'
+console.log(firstVowel('dog')); // 'o'
+console.log(firstVowel('conventional')); // 'o'
+console.log(firstVowel('rhythm')); // null
