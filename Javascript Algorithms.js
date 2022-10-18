b18edef6f33783e6d4a2fbf9e2d21816655631b4
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2022-10-18 23:40:51 +0200

    Update Javascript Algorithms.js

--- Javascript Algorithms.js
+++ Javascript Algorithms.js
@@ -236,3 +236,24 @@ console.log(firstVowel('dog')); // 'o'
 console.log(firstVowel('conventional')); // 'o'
 console.log(firstVowel('rhythm')); // null
 
+
+//Even Numbers
+//Write a function evenNumbers(max) that takes in a number as an arg. 
+//The function should return an array containing all positive, even numbers that are less than max. 
+
+// your code here
+function evenNumbers(max) {
+let arr = [];
+ for(let i = 1;  i < max; i++) {
+   if (i%2 === 0) {
+     arr.push(i)
+   }
+ }
+  
+  return arr;
+}
+
+
+console.log(evenNumbers(7)); // [ 2, 4, 6 ]
+console.log(evenNumbers(12)); // [ 2, 4, 6, 8, 10 ]
+console.log(evenNumbers(15)); // [ 2, 4, 6, 8, 10, 12, 14 ]
