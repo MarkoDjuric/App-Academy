41299446e84226dfbc3912699a02742c10f0a4de
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2022-10-19 22:22:32 +0200

    Update Javascript Algorithms.js

--- Javascript Algorithms.js
+++ Javascript Algorithms.js
@@ -280,3 +280,28 @@ console.log(lastVowel('TUNNEL')); // 'E'
 console.log(lastVowel('dog')); // 'o'
 console.log(lastVowel('conventional')); // 'a'
 console.log(lastVowel('rhythm')); // null
+
+
+//Pit Pat
+//Write a function pitPat(max) that accepts a number as an arg. 
+//The function should return an array containing all positive numbers less than or equal to max that are divisible by either 4 or 6, but not both.
+
+
+function pitPat(max) {
+  
+ let pitArr = [];
+  
+  for(let i = 1; i <= max; i++) {
+    
+   if((i % 4 == 0) || (i % 6 == 0) && !(i % 4 == 0) && (i % 6 == 0)) {
+     
+     pitArr.push(i)
+    }
+  }
+  
+  return pitArr
+}
+
+
+console.log(pitPat(18)); // [ 4, 6, 8, 16, 18 ]
+console.log(pitPat(30)); // [ 4, 6, 8, 16, 18, 20, 28, 30 ]
