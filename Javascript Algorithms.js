7c4e27c2ac4949ceeb3730724baa07d90b980f11
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2022-10-20 22:10:13 +0200

    Update Javascript Algorithms.js

--- Javascript Algorithms.js
+++ Javascript Algorithms.js
@@ -532,3 +532,34 @@ function sumArray(arr) {
 console.log(sumArray([4, 3, -1, 10])); // 16
 console.log(sumArray([6, 7, 2])); // 15
 console.log(sumArray([])); // 0
+
+//Avg Val
+//Write a function avgVal(arr) that accepts an array as an arg. The function should return the average of all values in the array. If the array is empty, it should return null.
+
+
+function avgVal(arr) {
+	let num = arr.length;
+  let avg = 0;
+  
+      if (arr.length === 0) {
+        return null;
+      }
+  
+ for(let i = 0; i < arr.length; i++) {
+   
+     avg += averageNum(arr[i])
+ }
+  return avg / num
+}
+
+function averageNum(sum) {
+  let allSum = 0; 
+  
+  allSum += sum;
+  return allSum;
+}
+
+
+console.log(avgVal([5, 10])); // 7.5
+console.log(avgVal([3, 7, 2, 1, 2])); // 3
+console.log(avgVal([])); // null
