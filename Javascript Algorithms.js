32dd51f0ef5a0989599f3b6928236cf41f3c52a3
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2022-10-20 22:05:23 +0200

    Update Javascript Algorithms.js

--- Javascript Algorithms.js
+++ Javascript Algorithms.js
@@ -461,3 +461,34 @@ let arr2 = [
     [3, 1, 2],
 ];
 //console.log(twoDimensionalSum(arr2)); // 6
+
+//Pig Latin Recall
+//Pig Latin is a fun take on the English language where you move any consonant cluster from the start of the word to the end of the word; when words begin on a vowel, you simply add "-yay". Vowels are "aeiou". 
+//Write a function pigLatinWord that takes in a word string and translates the word into Pig Latin. For this problem use the String's slice() method. The slice() method extracts a section of a string and returns it as a new string, without modifying the original string. 
+
+
+// So the two rules for our version of Pig Latin are:
+
+// 1. For words that start with a vowel, add 'yay' to the end of the word.
+// 2. For words that start with a non-vowel, move all letters that come
+// **before the first vowel** to the **end of the word** then add 'ay'
+
+function pigLatinWord(word) {
+  // your code here...
+  let vowels = "aeiou";
+  let noyayaed = "";
+  for(let i = 0; i < word.length; i++) {
+   if(vowels.includes(word[i])) {
+      if (i > 0) {
+         return word.slice(i) + word.slice(0,i) + 'ay'
+     }else {
+     return word + "yay"
+   }
+  }
+  }
+};
+
+console.log(pigLatinWord("apple")); //=> "appleyay"
+console.log(pigLatinWord("eat")); //=> "eatyay"
+console.log(pigLatinWord("banana")); //=> "ananabay"
+console.log(pigLatinWord("trash")); //=> "ashtray"
