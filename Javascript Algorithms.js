21af97c10e0f1a8dec0e1f1da9815865a9834f3b
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2022-10-24 23:12:47 +0200

    Update Javascript Algorithms.js

--- Javascript Algorithms.js
+++ Javascript Algorithms.js
@@ -590,3 +590,22 @@ console.log(myIndexOf(['a', 'b', 'c', 'e'], 'e')); // 3
 console.log(myIndexOf(['a', 'b', 'c', 'e'], 'z')); // -1
 console.log(myIndexOf([43, -7, 11, 13, 43], 43)); // 0
 console.log(myIndexOf([43, -7, 11, 13], 1)); // -1
+
+
+//Tripler
+//Write a function tripler(nums) that takes in an array as an arg. The function should return a new array containing three times every number of the original array.
+
+
+function tripler(nums) {
+ 	let tripleArr = [];
+  for(let i = 0; i < nums.length; i++) {
+          tripleArr.push(nums[i] * 3);
+  }
+  
+  return tripleArr;
+};
+
+
+
+console.log(tripler([2, 7, 4])); // [ 6, 21, 12 ]
+console.log(tripler([-5, 10, 0, 11])); // [ -15, 30, 0, 33 ]
