477582eb910c736f522d728dde89dd0215c4a747
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2022-10-24 23:11:33 +0200

    Update Javascript Algorithms.js

--- Javascript Algorithms.js
+++ Javascript Algorithms.js
@@ -563,3 +563,30 @@ function averageNum(sum) {
 console.log(avgVal([5, 10])); // 7.5
 console.log(avgVal([3, 7, 2, 1, 2])); // 3
 console.log(avgVal([])); // null
+
+//My Index Of Recall
+//Write a function myIndexOf(arr, target) that takes in an array and target value as args. 
+//The function should return the first index where the target is found in the array. 
+//If the target is not found, it should return -1. Solve this without using Array.indexOf.
+
+
+
+function myIndexOf(arr, target) {
+ 
+  for(let i = 0; i < arr.length; i++) {
+   		let char = arr[i]; 
+        if(char === target){
+          return i
+        }
+  }
+  
+  return -1;
+};
+
+
+
+console.log(myIndexOf(['a', 'b', 'c', 'e'], 'c')); // 2
+console.log(myIndexOf(['a', 'b', 'c', 'e'], 'e')); // 3
+console.log(myIndexOf(['a', 'b', 'c', 'e'], 'z')); // -1
+console.log(myIndexOf([43, -7, 11, 13, 43], 43)); // 0
+console.log(myIndexOf([43, -7, 11, 13], 1)); // -1
