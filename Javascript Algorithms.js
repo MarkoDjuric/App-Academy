36b629775cfedf9ed65a4fd126d4468a7e6fa0db
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2022-10-24 23:14:31 +0200

    Update Javascript Algorithms.js

--- Javascript Algorithms.js
+++ Javascript Algorithms.js
@@ -606,6 +606,26 @@ function tripler(nums) {
 };
 
 
-
 console.log(tripler([2, 7, 4])); // [ 6, 21, 12 ]
 console.log(tripler([-5, 10, 0, 11])); // [ -15, 30, 0, 33 ]
+
+
+//Long Words
+//Write a function longWords(words) that takes in an array of words. 
+//The function should return an array containing only the words that are longer than 5 characters.
+
+
+function longWords(words) {
+  let longArr = [];
+  for(let i = 0; i < words.length; i++) {
+      if(words[i].length > 5) {
+       		longArr.push(words[i]) 
+      }
+  }
+  
+  return longArr;
+};
+
+
+console.log(longWords(['bike', 'skateboard','scooter', 'moped'])); // [ 'skateboard', 'scooter' ]
+console.log(longWords(['couscous', 'soup', 'ceviche', 'solyanka' ,'taco'])); // [ 'couscous', 'ceviche', 'solyanka' ]
