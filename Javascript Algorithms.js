4632057751147c6d07a9db985819d1ce29976e3e
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2022-10-25 22:51:40 +0200

    Update Javascript Algorithms.js

--- Javascript Algorithms.js
+++ Javascript Algorithms.js
@@ -705,3 +705,25 @@ function reverseSentence(sentence) {
 console.log(reverseSentence('I am pretty hungry')); // 'hungry pretty am I'
 console.log(reverseSentence('follow the yellow brick road')); // 'road brick yellow the follow'
 
+//Initials
+//Write a function initials(name) that accepts a full name as an arg. The function should return the initials for that name.
+
+// your code here
+function initials(name) {
+ let nameArr = name.split(' ');
+ let initArr = [];
+ for(let i = 0; i < nameArr.length; i++) {
+    
+   initArr.push(nameArr[i][0].toUpperCase())
+ }
+  
+  return initArr.join("");
+  
+};
+
+
+
+console.log(initials('anna paschall')); // 'AP'
+console.log(initials('Mary La Grange')); // 'MLG'
+console.log(initials('brian crawford scott')); // 'BCS'
+console.log(initials('Benicio Monserrate Rafael del Toro Sánchez')); // 'BMRDTS'
