4ede64ab34c401e5ce972252b597cfc36c74a4b2
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2022-10-27 22:41:12 +0200

    Update Javascript Algorithms.js

--- Javascript Algorithms.js
+++ Javascript Algorithms.js
@@ -890,3 +890,23 @@ function longestWord(sentence) {
 console.log(longestWord('where did everyone go')); // 'everyone'
 console.log(longestWord('prefer simplicity over complexity')); // 'simplicity'
 console.log(longestWord('')); // ''
+
+
+//Product Array
+//Write a function productWithReduce(nums) that takes in an array of numbers. The function should return the total product of multiplying all numbers of the array together. You can assume that nums will not be an empty array.
+
+
+function productWithReduce(nums) {
+ 
+  let product = 1;
+  
+  for(let i = 0; i < nums.length; i++) {
+   product *= nums[i];
+  
+  }
+  
+  return product;
+
+}
+console.log(productWithReduce([10, 3, 5, 2])); // 300
+console.log(productWithReduce([4, 3])); // 12
