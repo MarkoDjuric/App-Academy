3fb3c53bf9e490b83535ac3dde5a1fa05dfce78d
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2022-10-30 20:54:31 +0100

    Update Javascript Algorithms.js

--- Javascript Algorithms.js
+++ Javascript Algorithms.js
@@ -1080,8 +1080,33 @@ function indexFinder(arr) {
 }
 
  
-
- 
 console.log(hipsterfy('wake up?')); // 'Whn shold everyon wak p?'
 console.log(hipsterfy('pmactoob')); // 'gt redy fr or bootcmp'
 console.log(hipsterfy('panthers are great animals')); // 'panthrs ar gret animls'
+
+
+//Least Common Multiple Recall
+//Write a function leastCommonMultiple(num1, num2) that accepts two numbers as arguments. The functions should return the smallest number that is divisible by both num1 and num2.
+
+
+function leastCommonMultiple(num1, num2) {
+ 
+  let i = 1;
+  let arr = [];
+  let loop = true;
+  
+  while(loop) {
+    if(i%num1 === 0 && i%num2 === 0) {
+      
+     	 loop = false;
+      break
+    }
+    i++;
+   }
+  return i;
+}
+
+
+console.log(leastCommonMultiple(4, 6)); // 12
+console.log(leastCommonMultiple(3, 5)); // 15
+console.log(leastCommonMultiple(2, 10)); // 10
