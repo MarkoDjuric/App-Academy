57333dbb1b34fcea300718e39285708997c6cf0f
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2022-10-30 20:58:21 +0100

    Update Javascript Algorithms.js

--- Javascript Algorithms.js
+++ Javascript Algorithms.js
@@ -1110,3 +1110,40 @@ function leastCommonMultiple(num1, num2) {
 console.log(leastCommonMultiple(4, 6)); // 12
 console.log(leastCommonMultiple(3, 5)); // 15
 console.log(leastCommonMultiple(2, 10)); // 10
+
+//Rotate
+//Write a function rotate(array, num) that takes in an array and a number as args. When the num is positive, the elements of the array should be rotated to the right. When the num is negative, the elements of the array should be rotated to the left. The function should mutate the original array. 
+
+
+
+function rotate(array, num) {
+  let poped;
+
+     if(num > 0) {
+
+       for(let i = 1; i <= num; i++){
+        
+         poped =  array.pop();
+         array.unshift(poped);
+      }
+  } else {
+    
+           for(let i = 0; i < -(num); i++){
+
+             let p = array.shift();
+              array.push(p);
+          }
+    
+        }
+   
+return array
+};
+
+
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+rotate(arr, 2);
+
+let animals = ['wombat', 'koala', 'opossum', 'kangaroo'];
+rotate(animals, -1);
+console.log(animals); // [ 'koala', 'opossum', 'kangaroo', 'wombat' ]
