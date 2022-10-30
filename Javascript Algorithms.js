506f5c2c75c65a403417be02d62d9a9e0fba3e21
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2022-10-30 21:03:11 +0100

    Update Javascript Algorithms.js

--- Javascript Algorithms.js
+++ Javascript Algorithms.js
@@ -1198,3 +1198,71 @@ console.log(words1); // [ 'BELKA', 'strelka', 'LAIKA', 'dezik', 'TSYGAN' ]
 let words2 = ['Yellowstone', 'Yosemite', 'Zion', 'Acadia'];
 alternatingWords(words2);
 console.log(words2); // [ 'YELLOWSTONE', 'yosemite', 'ZION', 'acadia' ]
+
+//Repeating Translate
+// Write a function `repeatingTranslate` that accepts a sentence as an argument.
+// The function should translate the sentence according to the following rules:
+//
+// - words that are shorter than 3 characters are unchanged
+// - words that are 3 characters or longer are translated according to the
+//   following rules:
+//   - if the word ends with a vowel, simply repeat the word twice (example:
+//     'like'->'likelike')
+//   - if the word ends with a non-vowel, repeat all letters that come after the
+//     word's last vowel, including the last vowel itself (example:
+//     'trash'->'trashash')
+//
+// Note that if words are capitalized in the original sentence, they should remain
+// capitalized in the translated sentence. Vowels are the letters a, e, i, o, u.
+
+function repeatingTranslate(sentence) {
+  let array = sentence.split(" ");
+  let vowels = 'aeiou';
+  let sent = " ";
+  
+  for(let i = 0; i < array.length; i++) {
+  		if(array[i].length > 2) {
+         	sent += isVowel(array[i])
+        }else {
+         sent += array[i] + " ";
+        }
+  }
+  
+	return sent
+}
+
+//Helper functions
+function isVowel(word) {
+  let vowels = 'aeiou';
+  let ind;
+  if(vowels.includes(word[word.length-1])) {
+    
+   		return word + word + " "; 
+    
+  }else {  
+     ind = indexVowel(word);
+    return 	word + word.slice(ind) + " ";
+  }
+  
+} 
+
+function indexVowel(char) {
+  let vowels = 'aeiou';
+ let i = char.length;
+  console.log(i)
+  while(i >= 0){
+    if(vowels.includes(char[i])) {
+    break  
+       
+    }
+    
+   i--; 
+  }
+  return i;
+}
+  
+   
+console.log(repeatingTranslate("we like to go running fast"));  // "we likelike to go runninging fastast"
+console.log(repeatingTranslate("he cannot find the trash"));    // "he cannotot findind thethe trashash"
+console.log(repeatingTranslate("pasta is my favorite dish"));   // "pastapasta is my favoritefavorite dishish"
+console.log(repeatingTranslate("her family flew to France"));   // "herer familyily flewew to FranceFrance"
