3d451e087e661bdaed8832035b2d3527c8415d59
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2022-10-31 20:43:34 +0100

    Update Javascript Algorithms.js

--- Javascript Algorithms.js
+++ Javascript Algorithms.js
@@ -1421,3 +1421,38 @@ function yeller(arr) {
 console.log(yeller(['hello', 'world'])); // [ 'HELLO!', 'WORLD!' ]
 console.log(yeller(['kiwi', 'orange', 'mango'])); // [ 'KIWI!', 'ORANGE!', 'MANGO!' ]
 
+//Choosey Endings
+//Write a function chooseyEndings that accepts an array of words and a suffix string as arguments. The function should return a new array containing the words that end in the given suffix. If the value passed in is not an array, return an empty array. HINT: There are built in JavaScript functions that will help with determining if a strings ends a certain way. Go see if you can find it on MDN!
+
+let chooseyEndings = function(words, suffix) {
+  if (!Array.isArray(words)) {
+    return [];
+  }
+
+  let filteredWords = [];
+
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i];
+    if (word.endsWith(suffix)) {
+      filteredWords.push(word);
+    }
+  }
+
+  return filteredWords;
+};
+
+
+console.log(chooseyEndings(['family', 'hound', 'catalyst','fly', 'timidly', 'bond'], 'ly'));
+// [ 'family', 'fly', 'timidly' ]
+
+console.log(chooseyEndings(['family', 'hound', 'catalyst','fly', 'timidly', 'bond'], 'nd'));
+// [ 'hound', 'bond' ]
+
+console.log(chooseyEndings(['simplicity', 'computer', 'felicity'], 'icity'));
+// [ 'simplicity', 'felicity' ]
+
+console.log(chooseyEndings(['simplicity', 'computer', 'felicity'], 'ily'));
+// [ ]
+
+console.log(chooseyEndings(17, 'ily'));
+// [ ]
