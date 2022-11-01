f859c265030d33ed7bb0a391420550c7765ac225
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2022-11-01 22:54:34 +0100

    Update Javascript Algorithms.js

--- Javascript Algorithms.js
+++ Javascript Algorithms.js
@@ -1352,7 +1352,7 @@ function maxColumn(matrix) {
     let rowMax = matrix[0][col];
     
         for (let i = 1 ; i < height; i++) {
-      //console.log(matrix[i][col])
+      
         if (matrix[i][col] > rowMax) {
             rowMax = matrix[i][col];
         }
@@ -1456,3 +1456,36 @@ console.log(chooseyEndings(['simplicity', 'computer', 'felicity'], 'ily'));
 
 console.log(chooseyEndings(17, 'ily'));
 // [ ]
+
+
+//Common Factors
+//Write a function commonFactors that accepts two numbers as arguments. 
+//The function should return an array containing positive numbers that are able to divide both arguments.
+
+
+function commonFactors(num1, num2) {
+  let cmf = [];
+  let counter = 0;
+  if(num1 > num2) {
+    	counter = num1;
+  }else{
+      counter = num2;
+    }
+  
+  
+ let i  = 1;
+  while(i <= counter) {
+    if(num1%i === 0 && num2%i === 0 ) {
+    	cmf.push(i)  
+    }
+    i++;
+  }
+    return cmf
+}
+
+console.log(commonFactors(50, 30));            // [1, 2, 5, 10]
+console.log(commonFactors(8, 4));              // [1, 2, 4]
+console.log(commonFactors(4, 8));              // [1, 2, 4]
+console.log(commonFactors(12, 24));            // [1, 2, 3, 4, 6, 12]
+console.log(commonFactors(22, 44));            // [1, 2, 11, 22]
+console.log(commonFactors(7, 9));              // [1]
