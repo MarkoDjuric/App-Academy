49d8b1149918cd60d3ceda127690917505b2c28c
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2022-11-01 22:56:42 +0100

    Update Javascript Algorithms.js

--- Javascript Algorithms.js
+++ Javascript Algorithms.js
@@ -1520,3 +1520,27 @@ console.log(commonFactors(4, 8));              // [1, 2, 4]
 console.log(commonFactors(12, 24));            // [1, 2, 3, 4, 6, 12]
 console.log(commonFactors(22, 44));            // [1, 2, 11, 22]
 console.log(commonFactors(7, 9));              // [1]
+
+//First and Last
+//Write a function firstAndLast that takes in an array of numbers and returns the sum of the first and last element if there is an even number of elements in the array. If there is an odd number of elements in the array, then the function should return the difference between the first and last elements of the array.
+
+
+function firstAndLast(arr) {
+  	let	first = arr[0];
+   	let last = arr[arr.length-1];
+  if(arr.length%2 !== 0) {
+   		return first - last 
+    
+  }else {
+  		 return first + last 
+  }  
+  
+}
+
+
+console.log(firstAndLast([1, 2, 3, 4]));    // 5
+console.log(firstAndLast([1, 2, 3, 4, 5])); // -4
+console.log(firstAndLast([12, 5]));         // 17
+console.log(firstAndLast([12]));            // 0
+console.log(firstAndLast([7, 11, 3]));      // 4
+//
