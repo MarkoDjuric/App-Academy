3b1e2f4bf9ff5acd495d4aa8b918d2b4ddfa70db
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2022-11-05 14:56:29 +0100

    Update Javascript Algorithms.js

--- Javascript Algorithms.js
+++ Javascript Algorithms.js
@@ -1681,3 +1681,26 @@ console.log(snakeToCamel('snakes_go_hiss')); // 'SnakesGoHiss'
 console.log(snakeToCamel('say_hello_world')); // 'SayHelloWorld'
 console.log(snakeToCamel('app_academy_is_cool')); // 'AppAcademyIsCool'
 console.log(snakeToCamel('APp_ACADEMY_iS_cOol')); // 'AppAcademyIsCool'
+
+//Strange Sums
+//Write a function strangeSums that accepts an array of numbers as an argument. The method should return a count of the number of distinct pairs of elements that have a sum of zero. You may assume that the input array contains unique elements.
+
+function strangeSums(arr) {
+  
+ let count = 0;
+  for(let i = 0; i < arr.length; i++) {
+   		for(let j  = i + 1; j < arr.length; j++) {
+          if(arr[i] + arr[j] === 0) {
+                 count += 1;
+      	  }
+      	}
+  }
+  
+  return count;
+}
+
+console.log(strangeSums([2,-3, 3, 4, -2]));     // 2
+console.log(strangeSums([42, 3, -1, -42]));      // 1
+console.log(strangeSums([-5, 5]));               // 1
+console.log(strangeSums([19, 6, -3, -20]));      // 0
+console.log(strangeSums([9]));                   // 0
