67f5e6689446bc6c19f563cdc74a310053dcd4d1
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2022-11-05 14:54:02 +0100

    Update Javascript Algorithms.js

--- Javascript Algorithms.js
+++ Javascript Algorithms.js
@@ -1633,4 +1633,29 @@ console.log(reverb('DISH'));    // DISHISH
 console.log(reverb(197393));    // null
 
 
-//
+//Pair Product
+//Write a function pairProduct that accepts an array of numbers and a product as arguments. The function should return a boolean indicating whether or not a pair of distinct elements in the array result in the product when multiplied together. You may assume that the input array contains unique elements.
+
+
+function pairProduct(arr, num) {
+  
+ for(let i = 0; i < arr.length; i++) {
+   for(let j = i + 1; j < arr.length; j++) {
+     if(arr[i] * arr[j] === num) {
+      return true; 
+     }
+     
+   }
+  		  
+ }
+   
+  return false;
+}
+
+console.log(pairProduct([4, 2, 5, 8], 16))    // true
+console.log(pairProduct([8, 1, 9, 3], 8))     // true
+console.log(pairProduct([3, 4], 12))          // true
+console.log(pairProduct([3, 4, 6, 2, 5], 12)) // true
+console.log(pairProduct([4, 2, 5, 7], 16))    // false
+console.log(pairProduct([8, 4, 9, 3], 8))     // false
+console.log(pairProduct([3], 12))             // false
