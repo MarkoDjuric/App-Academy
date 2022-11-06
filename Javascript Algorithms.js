f095e4399978f2f8b635b6201c20776c1119ba19
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2022-11-06 09:08:33 +0100

    Update Javascript Algorithms.js

--- Javascript Algorithms.js
+++ Javascript Algorithms.js
@@ -1740,3 +1740,30 @@ console.log(tripletTrue('terrrrrible'));   // true
 console.log(tripletTrue('runninggg'));     // true
 console.log(tripletTrue('bootcamp'));      // false
 console.log(tripletTrue('e'));             // false
+
+
+//Three Increasing
+//Write a function threeIncreasing that accepts an array of numbers as an argument. The function should return a boolean indicating whether or not the array contains three consecutive numbers in consecutive increasing order, like 7, 8, 9.
+
+
+function threeIncreasing(arr) {
+  let counter = 0;
+    for(let i = 0; i < arr.length; i++) {
+          console.log(arr[i])
+
+		for(let j = i+1; j < arr.length; j++) {
+         // console.log(arr[j])
+   				if((arr[j]-arr[i] === 1) && (arr[j+1]-arr[j] === 1)) {
+                     return true 
+                } 
+        }	
+  }
+  
+  	return false
+};
+
+
+console.log(threeIncreasing([3, 2, 11, 12, 13, 2, 4]));     // true
+console.log(threeIncreasing([2, 7, 8, 9]));                 // true
+console.log(threeIncreasing([7, 2, 4, 5, 2, 1, 6]));        // false
+console.log(threeIncreasing([1, 2, 4, 5, 2, 7, 8]));        // false
