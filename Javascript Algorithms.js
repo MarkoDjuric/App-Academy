bfbbca2474747511ae10d612058046d5afe5b331
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2022-11-07 19:10:05 +0100

    Update Javascript Algorithms.js

--- Javascript Algorithms.js
+++ Javascript Algorithms.js
@@ -1852,3 +1852,27 @@ console.log(zanyZip([1, 2, 3, 4], ['eins', 'zwei', 'drei']));
 
 console.log(zanyZip(['alef', 'bet'], ['alpha', 'beta']));
 // [ [ 'alef', 'alpha' ], [ 'bet', 'beta' ] ]
+
+//Silly Strings
+//Write a function sillyString that accepts a word as an argument. The functions should return a new word where every vowel of the original word is followed by 'b' and that same vowel. For example, 'siren' would turn into 'sibireben'.
+
+let sillyString = function(word) {
+    let vowels = "aeiou";
+    let newWord = "";
+    for (let i = 0; i < word.length; i++) {
+      let char = word[i];
+      if (vowels.includes(char)) {
+        newWord += char + "b" + char;
+      } else {
+        newWord += char;
+      }
+    }
+    return newWord;
+};
+
+console.log(sillyString('stop'));       // stobop
+console.log(sillyString('that'));       // thabat
+console.log(sillyString('can'));        // caban
+console.log(sillyString('cats'));       // cabats
+console.log(sillyString('italy'));      // ibitabaly
+console.log(sillyString('scooter'));    // scobooboteber
