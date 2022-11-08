2d031864f83ad1a84f44cfdfc65fe90e4c658ca6
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2022-11-08 18:53:28 +0100

    Update Javascript Algorithms.js

--- Javascript Algorithms.js
+++ Javascript Algorithms.js
@@ -1933,3 +1933,34 @@ console.log(nextPrime(7)); // 11
 console.log(nextPrime(8)); // 11
 console.log(nextPrime(20)); // 23
 console.log(nextPrime(97)); // 101
+
+//Matrix Addition
+//A 2-dimensional array is also known as a "matrix". Write a function matrixAddition that accepts two matrices as arguments. 
+//The two matrices are guaranteed to have the same "height" and "width". The function should return a new matrix representing the sum of the two arguments.
+//To add matrices, we add the values at the same positions.
+
+
+function matrixAddition(mxA, mxB) {
+  let sum = [];
+  for(let i = 0; i < mxA.length; i++ ) {
+        let first = mxA[i].length;
+                sum.push([]);
+        for(let k = 0; k < first; k++ ) { 
+        	  sum[i].push(mxA[i][k] + mxB[i][k]);
+          }
+     }
+    
+  return sum;
+}
+
+
+let matrixA = [[2,5], [4,7]]
+let matrixB = [[9,1], [3,0]]
+let matrixC = [[-1,0], [0,-1]]
+let matrixD = [[2, -5], [7, 10], [0, 1]]
+let matrixE = [[0 , 0], [12, 4], [6,  3]]
+
+console.log(matrixAddition(matrixA, matrixB)); // [[11, 6], [7, 7]]
+console.log(matrixAddition(matrixA, matrixC)); // [[1, 5], [4, 6]]
+console.log(matrixAddition(matrixB, matrixC)); // [[8, 1], [3, -1]]
+console.log(matrixAddition(matrixD, matrixE)); // [[2, -5], [19, 14], [6, 4]]
