3ded4c690d905a77f54b2a97675489341caff94a
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2022-11-08 18:55:06 +0100

    Update Javascript Algorithms.js

--- Javascript Algorithms.js
+++ Javascript Algorithms.js
@@ -1964,3 +1964,29 @@ console.log(matrixAddition(matrixA, matrixB)); // [[11, 6], [7, 7]]
 console.log(matrixAddition(matrixA, matrixC)); // [[1, 5], [4, 6]]
 console.log(matrixAddition(matrixB, matrixC)); // [[8, 1], [3, -1]]
 console.log(matrixAddition(matrixD, matrixE)); // [[2, -5], [19, 14], [6, 4]]
+
+
+//Is Perfect Square
+//Write a function isPerfectSquare that accepts a number as an argument. 
+//The method should return a boolean indicating whether or not the argument is a perfect square.
+//A perfect square is a number that is the product of some number multiplied by itself.
+//For example, since 64 = 8 * 8 and 144 = 12 * 12, 64 and 144 are perfect squares; 35 is not a perfect square.
+
+let isPerfectSquare = function(n) {
+    for (let i = 1; i <= n; i++) {
+        if (i * i === n) {
+            return true;
+        }
+    }
+    return false;
+};
+
+console.log(isPerfectSquare(1))     // true
+console.log(isPerfectSquare(4))     // true
+console.log(isPerfectSquare(64))    // true
+console.log(isPerfectSquare(100))   // true
+console.log(isPerfectSquare(169))   // true
+console.log(isPerfectSquare(2))     // false
+console.log(isPerfectSquare(40))    // false
+console.log(isPerfectSquare(32))    // false
+console.log(isPerfectSquare(50))    // false
