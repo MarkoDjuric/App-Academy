8c8d6be216177455da59aeb6fb47d960175b1c39
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2022-11-10 17:20:44 +0100

    Update Javascript Algorithms.js

--- Javascript Algorithms.js
+++ Javascript Algorithms.js
@@ -2095,3 +2095,31 @@ console.log(hasThreeVowels('bootcamp prep'));   //  true
 console.log(hasThreeVowels('bootcamp'));        //  false
 console.log(hasThreeVowels('dog'));             //  false
 console.log(hasThreeVowels('go home'));         //  false
+
+//Fibonacci Sequence
+//Write a function fibonacciSequence that accepts a number as an argument. The function should return an array representing the fibonacci sequence up to the given length. The first and second numbers of the sequence are 1 and 1. To generate subsequent numbers of the sequence, we take the sum of the previous two numbers of the sequence.
+
+function fibonacciSequence(num) {
+  if (num === 0) {
+    return []; 
+  }else if (num === 1) {
+   return [1]; 
+  }
+  
+ 	let fib = [1,1];
+    let sum = 0;
+	for(let i = 0; i < num-2; i++) {
+      sum = fib[i] + fib[i + 1]
+      	fib.push(sum);
+    }
+      
+return fib
+};
+
+
+console.log(fibonacciSequence(4));  // [ 1, 1, 2, 3 ]
+console.log(fibonacciSequence(5));  // [ 1, 1, 2, 3, 5 ]
+console.log(fibonacciSequence(8));  // [ 1, 1, 2, 3, 5, 8, 13, 21 ]
+console.log(fibonacciSequence(0));  // [ ]
+console.log(fibonacciSequence(1));  // [ 1 ]
+console.log(fibonacciSequence(2));  // [ 1, 1 ]
