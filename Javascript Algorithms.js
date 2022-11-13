9a3d9d28370b47dfa5f0ca2e3ff29b81da11ed98
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2022-11-13 19:59:38 +0100

    Update Javascript Algorithms.js

--- Javascript Algorithms.js
+++ Javascript Algorithms.js
@@ -2225,3 +2225,44 @@ console.log(p2);
 //   [ 4, 4, 4 ],
 //   [ 2, 2, 2, 2 ]
 // ]
+
+
+//Fib
+//Write a function fib that accepts a number n as an argument. The function should return the "n-th" number of the Fibonacci sequence. 
+//The first two numbers of the Fibonacci sequence are 1; to generate subsequent numbers of the Fibonacci sequence, we take the sum of the previous two numbers of the sequence.
+
+
+// Your code here
+function fib(num) {
+  
+  if(num === 1) {
+   return 1; 
+    
+  }
+  
+  
+ let first = 1;
+ let last = 1;
+ let count = 1;
+  
+  for(let i = 1; i < num-1; i++) {
+    
+   count = first + last;
+    first = last;
+    last = count; 
+  }
+  
+  return count;
+  
+}
+
+
+console.log(fib(1));    // 1
+console.log(fib(2));    // 1
+console.log(fib(3));    // 2
+console.log(fib(4));    // 3
+console.log(fib(5));    // 5
+console.log(fib(6));    // 8
+console.log(fib(10));   // 55
+console.log(fib(11));   // 89
+console.log(fib(12));   // 144
