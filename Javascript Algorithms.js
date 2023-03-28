b97471235db9348d7dfae0d3b6aa4d28b5c07ec3
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2023-03-28 21:16:16 +0200

    Update Javascript Algorithms.js

--- Javascript Algorithms.js
+++ Javascript Algorithms.js
@@ -2455,3 +2455,24 @@ echo("Mom!"); // => prints "MOM! ... Mom! ... mom!"
 echo("hey"); // => prints "HEY ... hey ... hey"
 echo("JUMp"); // => prints "JUMP ... JUMp ... jump"
 
+
+//Vowel Counter Recall
+//Write a function, countVowels(word), that takes in a string word and returns the number of vowels in the word.
+//Vowels are the letters "a", "e", "i", "o", "u".
+
+function countVowels(word) {
+ const vowels = "aeiou";
+ let counter = 0;
+ 
+ for(let i = 0; i < word.length; i++) {
+     let el =  word[i];
+    if (vowels.includes(el)) {
+        counter += 1;
+    } 
+ }
+ return counter;
+};
+
+console.log(countVowels("bootcamp")); // => 3
+console.log(countVowels("apple")); // => 2
+console.log(countVowels("pizza")); // => 2
