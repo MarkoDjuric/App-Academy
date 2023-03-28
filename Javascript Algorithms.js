84587031efe4fbd728f588e0e29c7ed7c26e2978
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2023-03-28 21:19:29 +0200

    Update Javascript Algorithms.js

--- Javascript Algorithms.js
+++ Javascript Algorithms.js
@@ -2520,3 +2520,25 @@ console.log(pigLatinWord("apple")); //=> "appleyay"
 console.log(pigLatinWord("eat")); //=> "eatyay"
 console.log(pigLatinWord("banana")); //=> "ananabay"
 console.log(pigLatinWord("trash")); //=> "ashtray"
+
+
+//My Index Of Recall
+//Write a function myIndexOf(arr, target) that takes in an array and target value as args. 
+//The function should return the first index where the target is found in the array.
+//If the target is not found, it should return -1. Solve this without using Array.indexOf.
+
+
+function myIndexOf(arr, target) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === target) {
+            return i;
+        } 
+    }
+    return -1;
+}
+
+console.log(myIndexOf(['a', 'b', 'c', 'e'], 'c')); // 2
+console.log(myIndexOf(['a', 'b', 'c', 'e'], 'e')); // 3
+console.log(myIndexOf(['a', 'b', 'c', 'e'], 'z')); // -1
+console.log(myIndexOf([43, -7, 11, 13, 43], 43)); // 0
+console.log(myIndexOf([43, -7, 11, 13], 1)); // -1
