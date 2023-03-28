cee89b787efaa513b4acfd15d8e383f6083be882
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2023-03-28 21:20:37 +0200

    Update Javascript Algorithms.js

--- Javascript Algorithms.js
+++ Javascript Algorithms.js
@@ -2542,3 +2542,30 @@ console.log(myIndexOf(['a', 'b', 'c', 'e'], 'e')); // 3
 console.log(myIndexOf(['a', 'b', 'c', 'e'], 'z')); // -1
 console.log(myIndexOf([43, -7, 11, 13, 43], 43)); // 0
 console.log(myIndexOf([43, -7, 11, 13], 1)); // -1
+
+
+//Two Sum Recall
+//Write a function twoSum(arr, target) that accepts an array and a target number as args. 
+//The function should a return a boolean indicating if two distinct numbers of the array add up to the target value.
+//You can assume that input array contains only unique numbers.
+
+// your code here
+function twoSum(arr, target) {
+    for (let i = 0; i < arr.length; i++) {
+        let num1 = arr[i];
+
+        for (let j = i + 1; j < arr.length; j++) {
+            let num2 = arr[j];
+
+            if (num1 + num2 === target) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+console.log(twoSum([1, 7, 3, 0, 2], 5)); // true
+console.log(twoSum([1, 7, 3, 0, 2], 6)); // false
+console.log(twoSum([4, 6, 2, 3], 8)); // true
+console.log(twoSum([4, 6, 2, 3], 11)); // false
