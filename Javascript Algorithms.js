9ebe53bad52bf9b893d724edcd97a5d46c839f96
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2023-03-29 21:01:56 +0200

    Update Javascript Algorithms.js

--- Javascript Algorithms.js
+++ Javascript Algorithms.js
@@ -2631,8 +2631,61 @@ function hipsterfy(str){
     return newStr;
 }
 
-//console.log(hipsterfy('everyone'));
-
 console.log(hipsterfy('When should everyone wake up?')); // 'Whn shold everyon wak p?'
 console.log(hipsterfy('get ready for our bootcamp')); // 'gt redy fr or bootcmp'
 console.log(hipsterfy('panthers are great animals')); // 'panthrs ar gret animls'
+
+//Repeating Translate Recall
+// Write a function `repeatingTranslate` that accepts a sentence as an argument.
+// The function should translate the sentence according to the following rules:
+//
+// - words that are shorter than 3 characters are unchanged
+// - words that are 3 characters or longer are translated according to the
+//   following rules:
+//   - if the word ends with a vowel, simply repeat the word twice (example:
+//     'like'->'likelike')
+//   - if the word ends with a non-vowel, repeat all letters that come after the
+//     word's last vowel, including the last vowel itself (example:
+//     'trash'->'trashash')
+//
+// Note that if words are capitalized in the original sentence, they should remain
+// capitalized in the translated sentence. Vowels are the letters a, e, i, o, u.
+//
+// Can you reduce the problem into helper functions?
+
+
+function repeatingTranslate(str){
+    let array = str.split(" ");
+    let repeatingStr = '';
+    for(let i = 0; i < array.length; i++) {
+        if(array[i].length >= 3) {
+            repeatingStr += removeLast(array[i]) + ' '
+        } else {
+            repeatingStr += array[i] + ' ';
+        }  
+    }
+    
+     function removeLast(str){
+        let vowels = 'aeiou';
+        let lastChar = str[str.length-1];
+        let tripledStr = '';
+        let noTripledStr = '';
+        if(vowels.includes(lastChar)){
+            for(let i = 0; i < 2; i++){
+                tripledStr += str;
+            } 
+            return tripledStr;
+        }
+            for(let j = 0; j < str.length; j++){
+                    if(vowels.includes(str[str.length - j])){
+                          return  noTripledStr = str + str.slice(str.length-j);
+                    }
+            }
+    }
+    return repeatingStr;
+}
+
+console.log(repeatingTranslate("we like to go running fast"));  // "we likelike to go runninging fastast"
+console.log(repeatingTranslate("he cannot find the trash"));    // "he cannotot findind thethe trashash"
+console.log(repeatingTranslate("pasta is my favorite dish"));   // "pastapasta is my favoritefavorite dishish"
+console.log(repeatingTranslate("her family flew to France"));   // "herer familyily flewew to FranceFrance"
