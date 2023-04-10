e4a94ed9c331d9c309869a647f5c023bf9a07d48
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2023-04-10 17:41:16 +0200

    Update 10-my-every.js

--- Callbacks/problems/10-my-every.js
+++ Callbacks/problems/10-my-every.js
@@ -21,8 +21,13 @@ console.log(myEvery(['book', 'door'], hasO));           // true
 console.log(myEvery(['book', 'door', 'pen'], hasO));    // false
 *******************************************************************************/
 
-let myEvery = function() {
-
+let myEvery = function(arr, cb) {
+    for(let i = 0; i < arr.length; i++){
+        if(!cb(arr[i])) {
+         return false;
+        }
+   }
+   return true;
 };
 
 
