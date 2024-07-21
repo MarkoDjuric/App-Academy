2b2a93b8a933e6d11e2fbbc6edb3c9a7d6ef381d
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2024-07-21 20:29:58 +0200

    Update Javascript Algorithms.js

--- Javascript Algorithms.js
+++ Javascript Algorithms.js
@@ -2745,3 +2745,25 @@ let arr2 = [
     [2]
 ];
 console.log(twoDimensionalProduct(arr2)); // 88
+
+// Max in Matrix
+// Write a function maxInMatrix(matrix) that takes in a 2-dimensional array (matrix) and returns the largest value in the matrix. The matrix contains at least one value.
+
+function maxInMatrix(matrix) {
+    let val = matrix[0][0];
+    for (let i = 0; i < matrix.length; i++) {
+        for (let k = 0; k < matrix[i].length; k++) {
+            if (matrix[i][k] >= val) {
+                val = matrix[i][k];
+            }
+        }
+    };
+    
+    return val;
+}
+
+matrix = [[11,  2,-99],
+          [20, 19, 10],
+          [47, 72, 56]]
+
+console.log(maxInMatrix(matrix)); // 72
