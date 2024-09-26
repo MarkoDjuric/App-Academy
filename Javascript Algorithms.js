400558df00d98c37e4fb6fa57196c28367bbe5de
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2024-09-26 15:45:08 +0200

    Update Javascript Algorithms.js

--- Javascript Algorithms.js
+++ Javascript Algorithms.js
@@ -2915,3 +2915,52 @@ matrix = [[ 5, 10,  8,  6],
           [21, 15, 19, 10]]
 
 console.log(luckyNumbers(matrix));// [10] 
+
+
+//Spiral Matrix
+//Write a function spiralOrder(matrix) that takes in a 2-dimensional array (matrix) and returns an array containing the elements in spiral order.
+
+
+function spiralOrder(matrix) {
+  
+  
+  let sizeOfMatrix = matrix.length;
+  let sizeOfSubMatrix = matrix[0].length;
+  let spiralArray = [];
+  let frtsSpliced = [];
+  let secondSpliced = [];
+  
+        
+    for (let k = 0; k < sizeOfSubMatrix-1; k++) {
+        
+        spiralArray.push(matrix[k].splice(-1)[0]);
+       
+    }
+  
+  let noArrMatrix = matrix[0];
+  
+  let extract = matrix.splice(-2,2);
+
+ 
+noArrMatrix = noArrMatrix.concat(extract[1].reverse())
+noArrMatrix = noArrMatrix.concat(extract[0])
+ 
+  return noArrMatrix
+  
+
+}
+
+
+matrix = [[ 1, 2, 3],
+          [ 4, 5, 6],
+          [ 7, 8, 9]]
+
+console.log(spiralOrder(matrix)); // [1,2,3,6,9,8,7,4,5]
+
+matrix = [[1, 2, 3, 4],
+          [5, 6, 7, 8],
+          [9,10,11,12]]
+
+
+console.log(spiralOrder(matrix)); // [1,2,3,4,8,12,11,10,9,5,6,7]
+
