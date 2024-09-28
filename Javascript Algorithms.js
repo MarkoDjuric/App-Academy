f93ce954195e62324b9151c46487d1f549d44011
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2024-09-28 13:10:11 +0200

    Update Javascript Algorithms.js

--- Javascript Algorithms.js
+++ Javascript Algorithms.js
@@ -3007,5 +3007,63 @@ console.log(p1);
 // ]
 
 
+// Pascal's Triangle
+//Pascal's triangle is a 2-dimensional array with the shape of a pyramid. The top of the pyramid is the number 1.
+//To generate further levels of the pyramid, every element is the sum of the element above and to the left with the element above and to the right.
+//Non-existing elements are treated as 0 when calculating the sum.
 
 
+function pascalsTriangle(num) {
+    
+    let base = [[1],[1,1]];
+    
+    let i  = 1;
+    let sum;
+ 
+    while (i < num-1) {
+        let tempArray = [1,1];
+        for (let j = 0; j < base.length-1; j++) {
+            sum = base[i][j] + base[i][j+1];
+            tempArray.splice(1,0,sum);
+        }
+        base.push(tempArray);
+        tempArray = [1,1];
+        i++
+    }
+ 
+    return base;
+}
+
+
+
+
+// For example, here are the first 5 levels of Pascal's triangle:
+//       1
+//      1 1
+//     1 2 1
+//    1 3 3 1
+//   1 4 6 4 1
+
+// Your code here
+
+
+
+//console.log(pascalsTriangle(5));
+// [
+//     [1],
+//     [1, 1],
+//     [1, 2, 1],
+//     [1, 3, 3, 1],
+//     [1, 4, 6, 4, 1]
+// ]
+
+console.log(pascalsTriangle(7));
+// [
+//     [1],
+//     [1, 1],
+//     [1, 2, 1],
+//     [1, 3, 3, 1],
+//     [1, 4, 6, 4, 1],
+//     [1, 5, 10, 10, 5, 1],
+//     [1, 6, 15, 20, 15, 6, 1]
+// ]
