da7ebfca9d6ae3586cea39a3df14dbb4b23edf07
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2024-09-29 15:58:14 +0200

    Update Javascript Algorithms.js

--- Javascript Algorithms.js
+++ Javascript Algorithms.js
@@ -3097,3 +3097,19 @@ console.log(pigLatinWord("apple")); //=> "appleyay"
 console.log(pigLatinWord("eat")); //=> "eatyay"
 console.log(pigLatinWord("banana")); //=> "ananabay"
 console.log(pigLatinWord("trash")); //=> "ashtray"
+
+//Sum Array Recall
+//Write a function sumArray(array) that takes in an array of numbers and returns the total sum of all the numbers.
+
+function sumArray(array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i]; 
+    }
+    
+    return sum;
+    
+}
+
+console.log(sumArray([5, 6, 4])); // => 15
+console.log(sumArray([7, 3, 9, 11])); // => 30
