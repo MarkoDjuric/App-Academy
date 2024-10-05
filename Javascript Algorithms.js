06fdf647822e4bc2e727c6dbc9d4a49f52045ab0
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2024-10-05 08:48:23 +0200

    Update Javascript Algorithms.js

--- Javascript Algorithms.js
+++ Javascript Algorithms.js
@@ -3233,3 +3233,35 @@ let longestWord = function(sentence) {
     });
     return reduced;
 };
+
+/*
+Write a function `hasThreeVowels` that accepts a string as an argument.
+The function should return a boolean indicating whether or not the string
+contains at least three different vowels.
+
+Solve this using Array's `forEach()`, `map()`, `filter()` **OR** `reduce()`
+methods.
+
+Examples:
+
+console.log(hasThreeVowels('delicious'));       //  true
+console.log(hasThreeVowels('bootcamp prep'));   //  true
+console.log(hasThreeVowels('bootcamp'));        //  false
+console.log(hasThreeVowels('dog'));             //  false
+console.log(hasThreeVowels('go home'));         //  false
+
+*/
+
+let hasThreeVowels = function(string) {
+    const vowels = 'aeiou';
+   let stringVowels = ['q'];
+    let arr = string.split("");
+ 
+ arr.map(function(el,idx){
+    if(vowels.includes(el)){
+        if(!stringVowels.includes(el)){
+           stringVowels.push(el);           
+        }
+    }
+});
+return stringVowels.length >= 4 
