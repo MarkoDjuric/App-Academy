6acb26255ea5c87529960a0ec3a525a69a272eca
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2024-11-13 21:17:10 +0100

    Update 11-flatten.js
    solved task

--- Recursion/starter/problems/11-flatten.js
+++ Recursion/starter/problems/11-flatten.js
@@ -11,7 +11,20 @@ flatten([1, [2, [3]]]); // [1, 2, 3]
 ***********************************************************************/
 
 // your code here
-  
+  function flatten(arr) {
+    if (arr.length === 0) {
+        return [];
+    }
+
+    let first = arr[0];
+
+    if (Array.isArray(first)) {
+        return flatten(first).concat(flatten(arr.slice(1)));
+    } else {
+        return [first].concat(flatten(arr.slice(1)));
+    }
+}
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = flatten;
