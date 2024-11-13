3b22395fe7f9b106da049ebee8092b988957be27
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2024-11-13 21:15:56 +0100

    Update 12-is-sorted.js
    solved task

--- Recursion/starter/problems/12-is-sorted.js
+++ Recursion/starter/problems/12-is-sorted.js
@@ -11,7 +11,32 @@ isSorted([5, 4, 3, 2, 1]); // false
 ***********************************************************************/
 
 // your code here
+function isSorted(arr) {
+    
+    let firstNum = arr[0];
+    let secondNum = arr[1];
+    
+    if(arr.length == 2) {
+        return true
+    }
+       if(secondNum - firstNum != 1) {
+        return false
+    }
+    
+    if(arr.length > 1) {
+         arr.splice(0,1)
+    }
+    
+    return   isSorted(arr) 
+};
 
+
+
+
+
+console.log(isSorted([1, 2, 3, 4, 5]));
+console.log(isSorted([1, 2, 4, 3, 5]));
+console.log(isSorted([2, 4, 6, 7, 8]));
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = isSorted;
